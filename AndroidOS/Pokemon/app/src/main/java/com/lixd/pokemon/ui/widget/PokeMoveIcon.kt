84743685e431928d532627376acfdf53954554d4
lixiaodong <it_lixd@163.com>
2024-09-30 19:29:38 +0800

    添加测试代码

---
+++ AndroidOS/Pokemon/app/src/main/java/com/lixd/pokemon/ui/widget/PokeMoveIcon.kt
@@ -0,0 +1,132 @@
+package com.lixd.pokemon.ui.widget
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.lixd.pokemon.util.AngleUtil
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
+
+/**
+ * 精力统计图标
+ */
+@Composable
+fun PokeMovesIcon(
+    size: Dp = 20.dp,
+    selectedSize: Dp = size.times(1.6f),
+    strokeWidth: Dp = 4.dp,
+    isSelected: Boolean = false,
+    color: Color = Color.White,
+    selectedColor: Color = Color.Black,
+    onClick: () -> Unit = {}
+) {
+    val starCount = 6
+    val currentColor = if (isSelected) selectedColor else color
+    val currentSize = if (isSelected) selectedSize else size
+    val animateSize by animateDpAsState(currentSize, label = "")
+    Box(modifier = Modifier
+        .size(animateSize)
+        .clickable { onClick() }) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val radiusOuter = min(this.size.width, this.size.height) / 2
+            val radiusInner = radiusOuter / 2 //五角星内圆半径
+            val startAngle = (-Math.PI / 2).toFloat() //开始绘制点的外径角度
+            val perAngle = (2 * Math.PI / starCount).toFloat() //两个五角星两个角直接的角度差
+            val outAngles = (0 until starCount).map {
+                val angle = it * perAngle + startAngle
+                Offset(radiusOuter * cos(angle), radiusOuter * sin(angle))
+            }//所有外圆角的顶点
+            val innerAngles = (0 until starCount).map {
+                val angle = it * perAngle + perAngle / 2 + startAngle
+                Offset(radiusInner * cos(angle), radiusInner * sin(angle))
+            }//所有内圆角的顶点
+            val path = Path()//绘制五角星的所有内圆外圆的点连接线
+            (0 until starCount).forEachIndexed { index, _ ->
+                val outerX = outAngles[index].x
+                val outerY = outAngles[index].y
+                val innerX = innerAngles[index].x
+                val innerY = innerAngles[index].y
+                if (index == 0) {
+                    path.moveTo(outerX, outerY)
+                    path.lineTo(innerX, innerY)
+                    path.lineTo(
+                        outAngles[(index + 1) % starCount].x,
+                        outAngles[(index + 1) % starCount].y
+                    )
+                } else {
+                    path.lineTo(innerX, innerY)//移动到内圆角的端点
+                    path.lineTo(
+                        outAngles[(index + 1) % starCount].x,
+                        outAngles[(index + 1) % starCount].y
+                    )//连接到下一个外圆角的端点
+                }
+                if (index == starCount - 1) {
+                    path.close()
+                }
+            }
+            rotate(15f) {
+                translate(this.size.width / 2, this.size.height / 2) {
+                    drawPath(path, currentColor, style = Stroke(width = strokeWidth.value))
+                }
+            }
+
+            val offset = 10f
+            val clearTopLeft = Offset(0f, center.y)
+            val clearRectSize = Size(center.x + offset, this.size.height)
+            drawRect(currentColor, clearTopLeft, clearRectSize, blendMode = BlendMode.Clear)
+
+            val rectWidth = (this.size.width / 2) * 0.8f
+            val rectHeight = (this.size.height / 2) * 0.5f
+            val rectTopLeft = Offset(offset, center.y + offset)
+            val rectSize = Size(rectWidth, rectHeight)
+            drawRoundRect(
+                currentColor, rectTopLeft, rectSize, cornerRadius = CornerRadius(8f, 8f),
+                style = Stroke(width = strokeWidth.value)
+            )
+
+//            val smallRectHeight = rectHeight * 0.2f
+//            val top = rectTopLeft - smallRectHeight
+//            drawLine(
+//                currentColor,
+//                start = Offset(rectTopLeft.x, top),
+//                end = Offset(rectSize.width, top),
+//                strokeWidth = strokeWidth.value
+//            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PokeMovesIconPreview() {
+    PokeMovesIcon(40.dp, isSelected = false)
+}
+
+@Preview
+@Composable
+fun PokeMovesIconPreview2() {
+    PokeMovesIcon(40.dp, isSelected = true)
+}
